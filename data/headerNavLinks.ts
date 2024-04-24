d10aa4c8df9be4935733e5889afbcac86fa09cc6
Manish Tripathy <mtripathy@palo-it.com>
2024-04-24 17:35:47 +0800

    Initial commit

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  // { href: '/projects', title: 'Projects' }, // TODO - Uncomment this after adding projects
+  { href: '/about', title: 'About' },
+]
+
+export default headerNavLinks
