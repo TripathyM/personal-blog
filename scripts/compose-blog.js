50e680951aedd4117d3da58bc249912cff283bc2
Manish Tripathy <mtripathy@palo-it.com>
2024-04-25 18:37:41 +0800

    Prettier fix

--- scripts/compose-blog.js
+++ scripts/compose-blog.js
@@ -1,16 +1,16 @@
 // scripts/createPost.js
-const fs = require("fs");
-const path = require("path");
-const readline = require("readline");
+const fs = require('fs')
+const path = require('path')
+const readline = require('readline')
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-});
+})
 
-rl.question("Enter the title of your new post: ", (title) => {
-  const slug = title.toLowerCase().replace(/ /g, "-");
-  const date = new Date().toISOString().split("T")[0];
+rl.question('Enter the title of your new post: ', (title) => {
+  const slug = title.toLowerCase().replace(/ /g, '-')
+  const date = new Date().toISOString().split('T')[0]
   const template = `---
 title: '${title}'
 date: '${date}'
@@ -20,25 +20,23 @@ summary: ''
 ---
 
 Enter your post content here.
-`;
+`
 
-  const blogDir = path.join(__dirname, "../data/blog");
+  const blogDir = path.join(__dirname, '../data/blog')
   fs.readdir(blogDir, (err, files) => {
-    if (err) throw err;
+    if (err) throw err
 
-    const sequenceNumbers = files.map((file) => parseInt(file.split("-")[0]));
-    const maxSequenceNumber = Math.max(...sequenceNumbers);
-    const newSequenceNumber = isNaN(maxSequenceNumber)
-      ? 1
-      : maxSequenceNumber + 1;
+    const sequenceNumbers = files.map((file) => parseInt(file.split('-')[0]))
+    const maxSequenceNumber = Math.max(...sequenceNumbers)
+    const newSequenceNumber = isNaN(maxSequenceNumber) ? 1 : maxSequenceNumber + 1
 
-    const filename = `${newSequenceNumber.toString().padStart(2, "0")}_${slug}.mdx`;
+    const filename = `${newSequenceNumber.toString().padStart(2, '0')}_${slug}.mdx`
 
     fs.writeFile(path.join(blogDir, filename), template, (err) => {
-      if (err) throw err;
-      console.log(`Blog post created: ${filename}`);
-    });
-  });
+      if (err) throw err
+      console.log(`Blog post created: ${filename}`)
+    })
+  })
 
-  rl.close();
-});
+  rl.close()
+})
